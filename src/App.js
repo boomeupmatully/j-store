196d69ae861bbcdee9bd2ca42d7dfaf2649c9280
Jason <jasonrwhite1983@gmail.com>
2019-07-20 04:11:47 -0400

    Added Router Code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
+
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path = '/' component={HomePage} />
+        <Route  path = '/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
