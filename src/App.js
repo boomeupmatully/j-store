a3168ab373bb6f9d107f84bf189d403dfd2f8d6e
Jason <jasonrwhite1983@gmail.com>
2019-07-22 01:58:27 -0400

    added redux and only show sign in when signed out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import {connect} from 'react-redux';
 import './App.css';
 
 
@@ -9,37 +10,30 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
 
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
           const userRef = await createUserProfileDocument(userAuth); 
 
           userRef.onSnapshot(snapShot => {
-              this.setState({
-                currentUser:{
+              setCurrentUser({
                   id: snapShot.id,
                   ...snapShot.data()
-                }
-              }, () =>{
-                console.log(this.state);
-              }
-              )
+              });
+
           });
   
       }
-      this.setState({currentUser: userAuth});     
+      console.log(userAuth);
+      setCurrentUser(userAuth);   
     })
   }
 
@@ -49,15 +43,26 @@ class App extends React.Component {
 
   render() {
     return(<div>
-      <Header currentUser = {this.state.currentUser} />
+      <Header />
       <Switch>
         <Route exact path = '/' component={HomePage} />
         <Route  path = '/shop' component={ShopPage} />
-        <Route  path = '/signin' component={SignInAndSignUpPage} />
+        <Route  path = '/signin' render={() => this.props.currentUser ? 
+                                        (<Redirect to = '/' />) :
+                                        (<SignInAndSignUpPage />)} 
+        />
       </Switch>
     </div>
     )
   }
 }
 
-export default App;
+const mapStateToProps = ({user}) =>({
+    currentUser: user.currentUser
+})
+
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './header.styles.scss';
 import  {Link} from 'react-router-dom';
+import {connect}  from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
@@ -28,5 +29,8 @@ const Header = ({currentUser}) =>(
         </div>
     </div>
 )
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
 
-export default Header;
+export default connect(mapStateToProps)(Header);
